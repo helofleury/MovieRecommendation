79a18c3f549618399383c14098ac50c28276a533
Heloisa <heloisafleury012@gmail.com>
2026-03-22 21:32:51 -0300

    setup FastAPI application entry point and register routes

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.db.database import engine, Base
+
+from app.api.routes import movies, ratings, users
+
+# cria tabelas
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Movie Recommendation API")
+
+app.include_router(users.router)
+app.include_router(movies.router)
+app.include_router(ratings.router)
+
+@app.get("/")
+def root():
+    return {"message": "API rodando 🚀"}
